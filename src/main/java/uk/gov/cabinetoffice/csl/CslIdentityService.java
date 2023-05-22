13d7e308292ad831af1fbba8b5efcd7f15bb0116
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-05-22 13:58:46 +0100

    LC-2026 base code for csl-identity-service

---
+++ src/main/java/uk/gov/cabinetoffice/csl/CslIdentityService.java
@@ -0,0 +1,11 @@
+package uk.gov.cabinetoffice.csl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CslIdentityService {
+	public static void main(String[] args) {
+		SpringApplication.run(CslIdentityService.class, args);
+	}
+}
