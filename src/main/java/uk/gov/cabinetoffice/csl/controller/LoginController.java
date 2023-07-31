0d0cb0ed59c6a69bca4087f04123b340966d0a53
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-07-31 11:27:23 +0100

    LC-2046 code cleanup

--- src/main/java/uk/gov/cabinetoffice/csl/controller/LoginController.java
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/LoginController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
@@ -17,7 +16,7 @@ public class LoginController {
   private String lpgUiUrl;
 
   @RequestMapping("/login")
-  public String login(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
+  public String login(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     DefaultSavedRequest dsr =
         (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
