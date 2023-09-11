d8dc3c81b65e5b3f6b851ecb16b0e27410bbce16
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-09-11 17:48:54 +0100

    LC-2037 Implement Client  DB-repository

--- src/test/java/uk/gov/cabinetoffice/csl/CslIdentityServiceTest.java
+++ src/test/java/uk/gov/cabinetoffice/csl/CslIdentityServiceTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("no-security")
 class CslIdentityServiceTest {
 
     @Autowired
