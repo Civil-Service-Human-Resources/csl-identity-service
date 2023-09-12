c5f80135fb286267819f6e0ab7dcc0685f946a7c
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-09-12 12:24:57 +0100

    LC-2037 default DB set to in-memory DB

--- src/test/java/uk/gov/cabinetoffice/csl/CslIdentityServiceTest.java
+++ src/test/java/uk/gov/cabinetoffice/csl/CslIdentityServiceTest.java
@@ -5,20 +5,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-@ActiveProfiles("no-security")
 class CslIdentityServiceTest {
 
     @Autowired
     private ConfigurableApplicationContext context;
 
-    //@Test
+    @Test
     void testApplicationContextActive() {
         assertTrue(context.isActive());
     }
