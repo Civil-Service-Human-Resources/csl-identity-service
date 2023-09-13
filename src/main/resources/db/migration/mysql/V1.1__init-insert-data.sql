ca604454bbc96c265d5d20cb4159c39cd7a9d1e0
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-09-13 15:19:44 +0100

    LC-2037 client insert sql created

--- src/main/resources/db/migration/mysql/V1.1__init-insert-data.sql
+++ src/main/resources/db/migration/mysql/V1.1__init-insert-data.sql
@@ -56,6 +56,3 @@ NULL,
 "read,openid,write",
 "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":true,\"settings.client.require-authorization-consent\":false}",
 "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",43200.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"self-contained\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",86400.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.device-code-time-to-live\":[\"java.time.Duration\",300.000000000]}");
-
-
-
