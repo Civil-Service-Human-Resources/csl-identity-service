230c422b7e5403ddc8cda59d6fbbab94e9dfa5d4
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-09-14 16:50:57 +0100

    LC-2037 code cleanup

--- src/main/java/uk/gov/cabinetoffice/csl/config/SecurityConfig.java
+++ src/main/java/uk/gov/cabinetoffice/csl/config/SecurityConfig.java
@@ -28,13 +28,10 @@ import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.authorization.*;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -47,13 +44,8 @@ import uk.gov.cabinetoffice.csl.handler.WebSecurityExpressionHandler;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.time.Duration;
 import java.util.*;
 import java.util.stream.Collectors;
-
-import static org.springframework.security.oauth2.core.AuthorizationGrantType.*;
-import static org.springframework.security.oauth2.core.ClientAuthenticationMethod.CLIENT_SECRET_BASIC;
-import static org.springframework.security.oauth2.core.ClientAuthenticationMethod.CLIENT_SECRET_JWT;
 
 @Configuration
 public class SecurityConfig {
@@ -69,28 +61,10 @@ public class SecurityConfig {
 	@Value("${oauth2.jwtKey}")
 	private String jwtKey;
 
-//	@Value("${oauth2.accessTokenTTLSeconds}")
-//	private Long accessTokenTTLSeconds;
-//
-//	@Value("${oauth2.refreshTokenTTLSeconds}")
-//	private Long refreshTokenTTLSeconds;
-//
 	@Value("${oauth2.scope}")
 	private String accessTokenScope;
-//
-//	@Value("${oauth2.requireAuthorizationConsent}")
-//	private Boolean requireAuthorizationConsent;
-//
-//	@Value("${oauth2.requirePKCE}")
-//	private Boolean requirePKCE;
 
 	//TODO: Below test properties will be removed after database implementation
-//	@Value("${test.redirectUri}")
-//	private String testRedirectUri;
-//	@Value("${test.client_id}")
-//	private String testClientId;
-//	@Value("${test.client_secret}")
-//	private String testClientSecret;
 	@Value("${test.learner_user_id}")
 	private String learnerUserId;
 	@Value("${test.learner_user_password}")
@@ -166,34 +140,7 @@ public class SecurityConfig {
 
 	@Bean
 	public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
-		//If the database already has client data or does not require default settings, just inject a JdbcRegisteredClientRepository directly
 		return new JdbcRegisteredClientRepository(jdbcTemplate);
-
-//		Set<String> scopes = new HashSet<>(Arrays.asList(accessTokenScope.split("\\s*,\\s*")));
-//		RegisteredClient registeredClient =
-//			RegisteredClient.withId(UUID.randomUUID().toString())
-//				.clientId(testClientId)
-//				.clientSecret(passwordEncoder().encode(testClientSecret))
-//				.redirectUri(testRedirectUri)
-//				.scopes(s -> s.addAll(scopes))
-//				.clientAuthenticationMethods(methods -> {
-//					methods.add(CLIENT_SECRET_BASIC);
-//					methods.add(CLIENT_SECRET_JWT);})
-//				.authorizationGrantTypes(grantTypes -> {
-//					grantTypes.add(CLIENT_CREDENTIALS);
-//					grantTypes.add(AUTHORIZATION_CODE);
-//					grantTypes.add(REFRESH_TOKEN);
-//					grantTypes.add(JWT_BEARER);
-//					grantTypes.add(PASSWORD);})
-//				.clientSettings(clientSettings())
-//				.tokenSettings(tokenSettings())
-//				.build();
-//		JdbcRegisteredClientRepository registeredClientRepository = new JdbcRegisteredClientRepository(jdbcTemplate);
-//		RegisteredClient repositoryByClientId = registeredClientRepository.findByClientId(registeredClient.getClientId());
-//		if (repositoryByClientId == null) {
-//			registeredClientRepository.save(registeredClient);
-//		}
-//		return registeredClientRepository;
 	}
 
 	@Bean
@@ -256,22 +203,6 @@ public class SecurityConfig {
 	public JwtEncoder jwtEncoder() {
 		return new NimbusJwtEncoder(jwkSource());
 	}
-
-//	@Bean
-//	TokenSettings tokenSettings() {
-//		return TokenSettings.builder()
-//				.accessTokenTimeToLive(Duration.ofSeconds(accessTokenTTLSeconds))
-//				.refreshTokenTimeToLive(Duration.ofSeconds(refreshTokenTTLSeconds))
-//				.build();
-//	}
-//
-//	@Bean
-//	ClientSettings clientSettings() {
-//		return ClientSettings.builder()
-//				.requireAuthorizationConsent(requireAuthorizationConsent)
-//				.requireProofKey(requirePKCE)
-//				.build();
-//	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
