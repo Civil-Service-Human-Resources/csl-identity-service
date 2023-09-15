0f050cc76e07e55a6a47e8645ef9cb06ad8b8d2b
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-09-15 12:43:44 +0100

    LC-2037 Integration DB Slqs are added

---
+++ docs/mysql_dev/sql/V1.1__init-insert-oauth2-data_Integration.sql
@@ -0,0 +1,64 @@
+INSERT INTO `csl_identity`.`oauth2_registered_client`
+(
+    `id`,
+    `client_id`,
+    `client_id_issued_at`,
+    `client_secret`,
+    `client_secret_expires_at`,
+    `client_name`,
+    `client_authentication_methods`,
+    `authorization_grant_types`,
+    `redirect_uris`,
+    `post_logout_redirect_uris`,
+    `scopes`,
+    `client_settings`,
+    `token_settings`
+)
+VALUES
+(
+    "116852c8-f968-4dd1-9495-c0edf96f42e4",
+    "9fbd4ae2-2db3-44c8-9544-88e80255b56e",
+    CURRENT_TIMESTAMP,
+    "$2a$10$IQ7.gaYFuZ9Ta9lRb6Ft1OeTSqylNncskKx.Wj9/wxji8Zpxu5fBi",
+    NULL,
+    "client-1",
+    "client_secret_jwt,client_secret_basic",
+    "refresh_token,client_credentials,password,authorization_code,urn:ietf:params:oauth:grant-type:jwt-bearer",
+    "https://integration.learn.civilservice.gov.uk",
+    "https://integration.learn.civilservice.gov.uk",
+    "read,openid,write",
+    "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":true,\"settings.client.require-authorization-consent\":false}",
+    "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",43200.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"self-contained\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",86400.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.device-code-time-to-live\":[\"java.time.Duration\",300.000000000]}"
+);
+
+INSERT INTO `csl_identity`.`oauth2_registered_client`
+(
+    `id`,
+    `client_id`,
+    `client_id_issued_at`,
+    `client_secret`,
+    `client_secret_expires_at`,
+    `client_name`,
+    `client_authentication_methods`,
+    `authorization_grant_types`,
+    `redirect_uris`,
+    `post_logout_redirect_uris`,
+    `scopes`,
+    `client_settings`,
+    `token_settings`)
+VALUES
+(
+    "216852c8-f968-4dd1-9495-c0edf96f42e4",
+    "8fbd4ae2-2db3-44c8-9544-88e80255b56e",
+    CURRENT_TIMESTAMP,
+    "$2a$10$IQ7.gaYFuZ9Ta9lRb6Ft1OeTSqylNncskKx.Wj9/wxji8Zpxu5fBi",
+    NULL,
+    "client-2",
+    "client_secret_jwt,client_secret_basic",
+    "refresh_token,client_credentials,password,authorization_code,urn:ietf:params:oauth:grant-type:jwt-bearer",
+    "https://integration.learn.civilservice.gov.uk",
+    "https://integration.learn.civilservice.gov.uk",
+    "read,openid,write",
+    "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":true,\"settings.client.require-authorization-consent\":false}",
+    "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",43200.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"self-contained\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",86400.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.device-code-time-to-live\":[\"java.time.Duration\",300.000000000]}"
+);
