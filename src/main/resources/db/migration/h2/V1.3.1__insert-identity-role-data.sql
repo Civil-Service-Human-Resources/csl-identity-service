5a462f9e5accb00ab4698ba9244121827c422e4d
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-09-22 16:06:54 +0100

    LC-2039 User repository using db

--- src/main/resources/db/migration/h2/V1.3.1__insert-identity-role-data.sql
+++ src/main/resources/db/migration/h2/V1.3.1__insert-identity-role-data.sql
@@ -41,3 +41,15 @@ VALUES
     (true, false, 'organisation-reporter@domain.com', 'ef422d43-53f1-492a-a159-54b8c5348df8', '$2a$10$eEvWOZ/wXNwL11SaQNV2q.CjE3AoIaRCle2984yYEB8Ul7UbreZNC'),
     (true, false, 'profession-reporter@domain.com', '5b1a0e11-12f5-47a8-9fe2-e272184defc9', '$2a$10$eEvWOZ/wXNwL11SaQNV2q.CjE3AoIaRCle2984yYEB8Ul7UbreZNC'),
     (true, false, 'cshr-reporter@domain.com', 'c4cb1208-eca7-46a6-b496-0f6f354c6eac', '$2a$10$eEvWOZ/wXNwL11SaQNV2q.CjE3AoIaRCle2984yYEB8Ul7UbreZNC');
+
+INSERT INTO identity_role
+(
+    identity_id, role_id
+)
+VALUES
+    ((SELECT id FROM identity WHERE email = 'learner@domain.com'), (SELECT id FROM role WHERE name = 'LEARNER')),
+    ((SELECT id FROM identity WHERE email = 'course-manager@domain.com'), (SELECT id FROM role WHERE name = 'LEARNING_MANAGER')),
+    ((SELECT id FROM identity WHERE email = 'identity-manager@domain.com'), (SELECT id FROM role WHERE name = 'IDENTITY_MANAGER')),
+    ((SELECT id FROM identity WHERE email = 'organisation-reporter@domain.com'), (SELECT id FROM role WHERE name = 'ORGANISATION_REPORTER')),
+    ((SELECT id FROM identity WHERE email = 'profession-reporter@domain.com'), (SELECT id FROM role WHERE name = 'PROFESSION_REPORTER')),
+    ((SELECT id FROM identity WHERE email = 'cshr-reporter@domain.com'), (SELECT id FROM role WHERE name = 'CSHR_REPORTER'));
