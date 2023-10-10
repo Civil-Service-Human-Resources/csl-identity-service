ec9b46f6ec8f5204a0f1cdd9619f0f331916ce94
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-10-10 15:16:03 +0100

    LC-2044 User registration journey

--- src/main/java/uk/gov/cabinetoffice/csl/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/uk/gov/cabinetoffice/csl/handler/CustomAuthenticationSuccessHandler.java
@@ -3,7 +3,7 @@ package uk.gov.cabinetoffice.csl.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -13,14 +13,21 @@ import uk.gov.cabinetoffice.csl.service.security.IdentityService;
 import java.io.IOException;
 import java.time.Instant;
 
-@AllArgsConstructor
 @Configuration
 public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    @Value("${authenticationSuccess.targetUrl}")
+    private String authenticationSuccessTargetUrl;
+
     private final IdentityService identityService;
+
+    public CustomAuthenticationSuccessHandler(IdentityService identityService) {
+        this.identityService = identityService;
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        this.setDefaultTargetUrl(authenticationSuccessTargetUrl);
         if (authentication != null) {
             if (authentication.getPrincipal() != null) {
                 if (authentication.getPrincipal() instanceof IdentityDetails identityDetails){
