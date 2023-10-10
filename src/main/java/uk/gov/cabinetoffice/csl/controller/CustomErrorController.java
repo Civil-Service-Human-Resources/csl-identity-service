52a9a4d5aed78a840d369f77e348de17041de086
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-10-10 15:28:14 +0100

    LC-2044 User registration journey

--- src/main/java/uk/gov/cabinetoffice/csl/controller/CustomErrorController.java
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/CustomErrorController.java
@@ -11,19 +11,13 @@ import uk.gov.cabinetoffice.csl.util.ErrorPageMap;
 public class CustomErrorController implements ErrorController {
   private static final String GENERIC_ERROR = "/error";
 
-  @RequestMapping("/error")
+  @RequestMapping(GENERIC_ERROR)
   public String handleError(HttpServletRequest request) {
     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
     if (status != null) {
       Integer statusCode = Integer.valueOf(status.toString());
-        return ErrorPageMap.ERROR_PAGES
-                .getOrDefault(statusCode, GENERIC_ERROR);
+        return ErrorPageMap.ERROR_PAGES.getOrDefault(statusCode, GENERIC_ERROR);
     }
-    return GENERIC_ERROR;
-  }
-
-  public String getErrorPath() {
     return GENERIC_ERROR;
   }
 }
--- src/main/java/uk/gov/cabinetoffice/csl/util/ErrorPageMap.java
+++ src/main/java/uk/gov/cabinetoffice/csl/util/ErrorPageMap.java
@@ -1,18 +1,8 @@
 package uk.gov.cabinetoffice.csl.util;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 public final class ErrorPageMap {
-    public static final Map<Integer, String> ERROR_PAGES;
-    static {
-        Map<Integer, String> pagesMap = new HashMap<>();
-        pagesMap.put(400, "400");
-        pagesMap.put(401, "401");
-        pagesMap.put(403, "403");
-        pagesMap.put(404, "404");
-        pagesMap.put(500, "500");
-        ERROR_PAGES = Collections.unmodifiableMap(pagesMap);
-    }
+    public static final Map<Integer, String> ERROR_PAGES =
+            Map.of(400, "400", 401, "401", 403, "403", 404, "404", 500, "500");
 }
