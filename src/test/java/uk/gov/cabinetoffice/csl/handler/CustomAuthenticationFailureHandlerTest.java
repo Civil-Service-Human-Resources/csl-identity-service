48c8af08e5f3895499f3faa2f813a7a3ba34ad11
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-11-03 14:53:38 +0000

    LC-2255 Unit tests

--- src/test/java/uk/gov/cabinetoffice/csl/handler/CustomAuthenticationFailureHandlerTest.java
+++ src/test/java/uk/gov/cabinetoffice/csl/handler/CustomAuthenticationFailureHandlerTest.java
@@ -8,14 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.AuthenticationException;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
+import static java.net.URLEncoder.encode;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.*;
 
@@ -44,22 +39,17 @@ public class CustomAuthenticationFailureHandlerTest {
     }
 
     @Test
-    public void shouldSetErrorToDeactivatedOnAccountDeactivatedAndPendingReactivationExists() throws IOException, ServletException {
+    public void shouldSetErrorToDeactivatedOnAccountDeactivatedAndPendingReactivationExists()
+            throws IOException, ServletException {
         HttpServletResponse response = executeHandler("Pending reactivation already exists for user");
         verify(response).sendRedirect("/login?error=pending-reactivation");
     }
 
     @Test
-    public void shouldSetErrorToDeactivatedOnAccountDeactivated() throws IOException, ServletException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        AuthenticationException exception = mock(AuthenticationException.class);
-        String username = "learner@domain.com";
+    public void shouldSetErrorToDeactivatedOnAccountDeactivated() throws IOException, ServletException {
+        HttpServletResponse response = executeHandler("User account is deactivated");
         String encryptedUsername = "W+tehauG4VaW9RRQXwc/8e1ETIr28UKG0eQYbPX2oLY=";
-        when(exception.getMessage()).thenReturn("User account is deactivated");
-        when(request.getParameter("username")).thenReturn(username);
-        authenticationFailureHandler.onAuthenticationFailure(request, response, exception);
-        verify(response).sendRedirect("/login?error=deactivated&username=" + URLEncoder.encode(encryptedUsername, UTF_8));
+        verify(response).sendRedirect("/login?error=deactivated&username=" + encode(encryptedUsername, UTF_8));
     }
 
     private HttpServletResponse executeHandler(String message) throws IOException, ServletException {
@@ -67,6 +57,8 @@ public class CustomAuthenticationFailureHandlerTest {
         HttpServletResponse response = mock(HttpServletResponse.class);
         AuthenticationException exception = mock(AuthenticationException.class);
         when(exception.getMessage()).thenReturn(message);
+        String username = "learner@domain.com";
+        when(request.getParameter("username")).thenReturn(username);
         authenticationFailureHandler.onAuthenticationFailure(request, response, exception);
         return response;
     }
