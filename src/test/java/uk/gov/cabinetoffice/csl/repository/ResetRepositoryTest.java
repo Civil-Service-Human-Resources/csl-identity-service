626b0f6607a67b4db97257b5ead8875666f92a1e
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-01-15 10:20:49 +0000

    LC-2048-Implement forgot password journey

---
+++ src/test/java/uk/gov/cabinetoffice/csl/repository/ResetRepositoryTest.java
@@ -0,0 +1,52 @@
+package uk.gov.cabinetoffice.csl.repository;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.cabinetoffice.csl.domain.Reset;
+import uk.gov.cabinetoffice.csl.domain.ResetStatus;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+@Transactional
+public class ResetRepositoryTest {
+
+    public static final String CODE = "ABC123";
+    public static final String EMAIL = "test@example.com";
+
+    @Autowired
+    private ResetRepository resetRepository;
+
+    @Test
+    public void existsByCodeReturnsCorrectBoolean() {
+        resetRepository.save(createReset());
+
+        assertThat(resetRepository.existsByCode(CODE), equalTo(true));
+        assertThat(resetRepository.existsByCode("def567"), equalTo(false));
+    }
+
+    @Test
+    public void findByCodeShouldReturnCorrectCode() {
+        Reset expectedReset = createReset();
+        resetRepository.save(expectedReset);
+        Reset actualReset = resetRepository.findByCode(CODE);
+
+        assertThat(actualReset.getCode(), equalTo(expectedReset.getCode()));
+        assertThat(actualReset.getEmail(), equalTo(expectedReset.getEmail()));
+    }
+
+    private Reset createReset() {
+        Reset reset = new Reset();
+        reset.setCode(CODE);
+        reset.setEmail(EMAIL);
+        reset.setResetStatus(ResetStatus.PENDING);
+        reset.setRequestedAt(new Date());
+        return reset;
+    }
+
+}
