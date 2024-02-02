af30a236b3bff254a86b3810696b97ebaa723681
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-02-02 14:01:10 +0000

    LC-2752-Implement update password journey

--- src/main/java/uk/gov/cabinetoffice/csl/repository/Oauth2AuthorizationRepository.java
+++ src/main/java/uk/gov/cabinetoffice/csl/repository/Oauth2AuthorizationRepository.java
@@ -2,10 +2,12 @@ package uk.gov.cabinetoffice.csl.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.cabinetoffice.csl.domain.Oauth2Authorization;
 
 @Repository
 public interface Oauth2AuthorizationRepository extends CrudRepository<Oauth2Authorization, Long> {
 
+    @Transactional
     Long deleteByPrincipalName(String principalName);
 }
