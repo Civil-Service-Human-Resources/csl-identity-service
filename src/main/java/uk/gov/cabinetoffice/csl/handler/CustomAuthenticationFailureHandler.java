0160e0a20de8298ace8c90adb7fe71a647fb00b0
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-02-08 15:43:07 +0000

    LC-2052 Implement user locking

--- src/main/java/uk/gov/cabinetoffice/csl/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/uk/gov/cabinetoffice/csl/handler/CustomAuthenticationFailureHandler.java
@@ -40,6 +40,7 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
                             getEncryptedText(request.getParameter("username"), encryptionKey));
             default -> {
                 //TODO:
+                // Call UserAuthService.getIdentity() and debug to see if it returns Identity
                 // Increase number of failed attempt in the identity DB table
                 // If number of failed attempt reached the threshold then show the locked error message to the user
                 // response.sendRedirect("/login?error=locked");
