e9f1d3ed53ede9ad41fd3b6ab2302b0a67bf4c57
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-04-11 14:29:43 +0100

    LC-2050-Implement user invite function

--- src/main/java/uk/gov/cabinetoffice/csl/controller/error/CustomErrorController.java
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/error/CustomErrorController.java
@@ -1,22 +1,24 @@
 package uk.gov.cabinetoffice.csl.controller.error;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static jakarta.servlet.RequestDispatcher.ERROR_STATUS_CODE;
+import static uk.gov.cabinetoffice.csl.controller.error.ErrorPageMap.ERROR_PAGES;
+
 @Controller
 public class CustomErrorController implements ErrorController {
 
-  private static final String GENERIC_ERROR_PAGE_TEMPLATE = "/error";
+  private static final String GENERIC_ERROR_PAGE_TEMPLATE = "error/error";
 
   @RequestMapping("/error")
   public String handleError(HttpServletRequest request) {
-    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    Object status = request.getAttribute(ERROR_STATUS_CODE);
     if (status != null) {
       Integer statusCode = Integer.valueOf(status.toString());
-        return ErrorPageMap.ERROR_PAGES.getOrDefault(statusCode, GENERIC_ERROR_PAGE_TEMPLATE);
+        return ERROR_PAGES.getOrDefault(statusCode, GENERIC_ERROR_PAGE_TEMPLATE);
     }
     return GENERIC_ERROR_PAGE_TEMPLATE;
   }
--- src/main/java/uk/gov/cabinetoffice/csl/controller/error/ErrorPageMap.java
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/error/ErrorPageMap.java
@@ -4,5 +4,12 @@ import java.util.Map;
 
 public final class ErrorPageMap {
     public static final Map<Integer, String> ERROR_PAGES =
-            Map.of(400, "400", 401, "401", 403, "403", 404, "404", 500, "500");
+            Map.of(
+                    400, "error/400",
+                    401, "error/401",
+                    403, "error/403",
+                    404, "error/404",
+                    500, "error/500",
+                    503, "error/503"
+            );
 }
