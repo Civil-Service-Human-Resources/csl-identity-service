c64781866b81cc2cc169c7be12f2b341113cdaac
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-04-22 12:04:04 +0100

    LC-2060 Update flyway DB Scripts to work with existing identity DB

--- src/main/resources/db/migration/mysql/V1.2.1__add-failed-login-attempts-to-identity-table.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE identity ADD failed_login_attempts smallint(5) DEFAULT 0;
--- src/main/resources/db/migration/mysql/V1.8.0__edit-email-update-table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE email_update RENAME COLUMN  email                   TO new_email;
-ALTER TABLE email_update RENAME COLUMN  timestamp               TO requested_at;
-ALTER TABLE email_update ADD            updated_at              datetime DEFAULT NULL;
-ALTER TABLE email_update ADD            previous_email          varchar(255) DEFAULT NULL;
-ALTER TABLE email_update ADD            email_update_status     varchar(10) DEFAULT NULL;
