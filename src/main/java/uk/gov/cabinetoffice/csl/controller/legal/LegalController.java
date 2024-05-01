e1270b7ba8d40eadb8450543cf93912a9662d559
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-05-01 21:21:24 +0100

    LC-2807-Fix the hardcoded Footer Links

---
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/legal/LegalController.java
@@ -0,0 +1,49 @@
+package uk.gov.cabinetoffice.csl.controller.legal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/legal")
+public class LegalController {
+
+    private static final String COOKIES_TEMPLATE = "legal/cookies";
+    private static final String PRIVACY_TEMPLATE = "legal/privacy";
+    private static final String CONTACT_US_TEMPLATE = "legal/contact-us";
+    private static final String ACCESSIBILITY_STATEMENT_TEMPLATE = "legal/accessibility-statement";
+
+    @Value("${lpg.contactEmail}")
+    private String contactEmail;
+
+    @Value("${lpg.contactNumber}")
+    private String contactNumber;
+
+    @GetMapping("/cookies")
+    public String cookiesPage() {
+        return COOKIES_TEMPLATE;
+    }
+
+    @GetMapping("/privacy")
+    public String privacyPage() {
+        return PRIVACY_TEMPLATE;
+    }
+
+    @GetMapping("/contact-us")
+    public String contactUs(Model model) {
+        model.addAttribute("contactEmail", contactEmail);
+        model.addAttribute("contactNumber", contactNumber);
+        return CONTACT_US_TEMPLATE;
+    }
+
+    @GetMapping("/accessibility-statement")
+    public String accessibilityStatement(Model model) {
+        model.addAttribute("contactEmail", contactEmail);
+        model.addAttribute("contactNumber", contactNumber);
+        return ACCESSIBILITY_STATEMENT_TEMPLATE;
+    }
+}
