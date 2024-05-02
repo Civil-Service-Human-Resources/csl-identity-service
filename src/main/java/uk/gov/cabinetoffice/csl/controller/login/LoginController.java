5ac6b8183252b9b916aae01b1c08ab22b0f65e41
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-05-02 18:14:34 +0100

    LC-2770-Bypass maintenance page for the defined users to test the release

--- src/main/java/uk/gov/cabinetoffice/csl/controller/login/LoginController.java
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/login/LoginController.java
@@ -4,10 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.cabinetoffice.csl.dto.IdentityDetails;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -42,15 +45,31 @@ public class LoginController {
   private String skipMaintenancePageForUsers;
 
   @RequestMapping("/login")
-  public String login(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
+  public String login(HttpServletRequest request, HttpServletResponse response,
+                      Model model, Authentication authentication) throws IOException {
 
     if(maintenancePageEnabled) {
+      log.info("LoginController.login.authentication: {}", authentication);
+      if (authentication != null) {
+        Object principal = authentication.getPrincipal();
+        log.info("LoginController.login.principal: {}", principal);
+        if(principal instanceof IdentityDetails identityDetails) {
+          log.info("LoginController.login.identityDetails: {}", identityDetails);
+        }
+        if(principal instanceof Jwt jwt) {
+          log.info("LoginController.login.jwt: {}", jwt);
+          String email = jwt.getClaim("email");
+          log.info("LoginController.login.email: {}", email);
+        }
+      }
+
       String skipMaintenancePageForUser = request.getParameter(SKIP_MAINTENANCE_PAGE_PARAM_NAME);
       log.info("LoginController.login.skipMaintenancePageForUser.username: {}", skipMaintenancePageForUser);
 
       boolean skipMaintenancePage = isNotBlank(skipMaintenancePageForUser) &&
               Arrays.stream(skipMaintenancePageForUsers.split(","))
               .anyMatch(u -> u.trim().equalsIgnoreCase(skipMaintenancePageForUser.trim()));
+      log.info("LoginController.login.skipMaintenancePage: {}", skipMaintenancePage);
 
       if (!skipMaintenancePage) {
         model.addAttribute("maintenancePageContentLine1", maintenancePageContentLine1);
