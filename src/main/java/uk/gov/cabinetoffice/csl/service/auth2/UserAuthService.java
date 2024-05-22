ec78efc5c7fc6fc502551a562a14ae473bb4cae5
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-05-22 13:52:57 +0100

    LC-2770-Bypass maintenance page for the defined users to test the release

--- src/main/java/uk/gov/cabinetoffice/csl/service/auth2/UserAuthService.java
+++ src/main/java/uk/gov/cabinetoffice/csl/service/auth2/UserAuthService.java
@@ -1,16 +1,12 @@
 package uk.gov.cabinetoffice.csl.service.auth2;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import uk.gov.cabinetoffice.csl.domain.Identity;
 import uk.gov.cabinetoffice.csl.dto.IdentityDetails;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-
-@Slf4j
 @Component
 @AllArgsConstructor
 public class UserAuthService implements IUserAuthService {
@@ -44,9 +40,6 @@ public class UserAuthService implements IUserAuthService {
         } else if (principal instanceof Jwt jwt) {
             uid = jwt.getClaim("user_name");
         }
-        if (isBlank(uid)) {
-            log.warn("Learner uid is missing from authentication token. User is not logged-in.");
-        }
         return uid;
     }
 
@@ -59,9 +52,6 @@ public class UserAuthService implements IUserAuthService {
             email = getIdentityDetails().getIdentity().getEmail();
         } else if (principal instanceof Jwt jwt) {
             email = jwt.getClaim("email");
-        }
-        if (isBlank(email)) {
-            log.warn("Learner email id is missing from authentication token. User is not logged-in.");
         }
         return email;
     }
