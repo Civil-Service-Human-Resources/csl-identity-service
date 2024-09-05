7f7db357397e31db8bd765e4b49f5dd37ebe9ae5
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-09-05 13:24:26 +0100

    LC-3076 Email update fixed

--- src/test/java/uk/gov/cabinetoffice/csl/controller/emailupdate/EmailUpdateRequestWhileLoginEmailUpdateControllerTest.java
+++ src/test/java/uk/gov/cabinetoffice/csl/controller/emailupdate/EmailUpdateRequestWhileLoginEmailUpdateControllerTest.java
@@ -2,13 +2,11 @@ package uk.gov.cabinetoffice.csl.controller.emailupdate;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.cabinetoffice.csl.domain.Identity;
 import uk.gov.cabinetoffice.csl.service.EmailUpdateService;
@@ -28,7 +26,6 @@ import static uk.gov.cabinetoffice.csl.util.ApplicationConstants.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("no-redis")
 @WithMockCustomUser
 public class EmailUpdateRequestWhileLoginEmailUpdateControllerTest {
