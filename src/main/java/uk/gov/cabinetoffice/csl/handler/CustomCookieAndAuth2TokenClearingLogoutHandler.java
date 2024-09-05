1c521d652f797ee54ee0b1ad5504ef29e8cf36a3
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-09-05 12:12:37 +0100

    LC-3062 Code Testing

--- src/main/java/uk/gov/cabinetoffice/csl/handler/CustomCookieAndAuth2TokenClearingLogoutHandler.java
+++ src/main/java/uk/gov/cabinetoffice/csl/handler/CustomCookieAndAuth2TokenClearingLogoutHandler.java
@@ -1,6 +1,5 @@
 package uk.gov.cabinetoffice.csl.handler;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
