3fc07ffb9fe03b5e0fbc012677da8f337a740e52
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-09-05 12:11:37 +0100

    LC-3062 Code Testing

--- src/main/java/uk/gov/cabinetoffice/csl/util/LogoutUtil.java
+++ src/main/java/uk/gov/cabinetoffice/csl/util/LogoutUtil.java
@@ -1,6 +1,5 @@
 package uk.gov.cabinetoffice.csl.util;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,27 +20,23 @@ public class LogoutUtil {
     private final Oauth2AuthorizationRepository oauth2AuthorizationRepository;
 
     public void logout(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            if (request.getCookies() != null) {
-                for (Cookie cookie : request.getCookies()) {
-                    log.debug("LogoutUtil.Cookie: {}", cookie);
-                    String cookieName = cookie.getName();
-                    log.debug("LogoutUtil.cookieName: {}", cookieName);
-                    Cookie cookieToDelete = new Cookie(cookieName, null);
-                    cookieToDelete.setMaxAge(0);
-                    response.addCookie(cookieToDelete);
-                }
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                log.debug("LogoutUtil.Cookie: {}", cookie);
+                String cookieName = cookie.getName();
+                log.debug("LogoutUtil.cookieName: {}", cookieName);
+                Cookie cookieToDelete = new Cookie(cookieName, null);
+                cookieToDelete.setMaxAge(0);
+                response.addCookie(cookieToDelete);
             }
-            String uid = userAuthService.getUid();
-            log.debug("LogoutUtil.uid: {}", uid);
-            if (isNotBlank(uid)) {
-                Long n = oauth2AuthorizationRepository.deleteByPrincipalName(uid);
-                log.debug("LogoutUtil: {} Oauth2Authorization entries deleted from DB for principal_name {}", n, uid);
-            }
-            request.getSession().invalidate();
-            log.debug("LogoutUtil: Session is invalidated.");
-        } catch (Exception e) {
-            log.info("LogoutUtil.logout: Exception is raised but no action required: " + e.getMessage());
         }
+        String uid = userAuthService.getUid();
+        log.debug("LogoutUtil.uid: {}", uid);
+        if (isNotBlank(uid)) {
+            Long n = oauth2AuthorizationRepository.deleteByPrincipalName(uid);
+            log.debug("LogoutUtil: {} Oauth2Authorization entries deleted from DB for principal_name {}", n, uid);
+        }
+        request.getSession().invalidate();
+        log.debug("LogoutUtil: Session is invalidated.");
     }
 }
