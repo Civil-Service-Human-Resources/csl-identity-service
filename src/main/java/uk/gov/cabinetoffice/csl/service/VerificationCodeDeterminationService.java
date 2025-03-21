218609dcf7580abb984943a59c70b3a2cae6e73a
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-03-21 12:29:12 +0000

    LC-3466-Update the login process for the blocked token users

--- src/main/java/uk/gov/cabinetoffice/csl/service/VerificationCodeDeterminationService.java
+++ src/main/java/uk/gov/cabinetoffice/csl/service/VerificationCodeDeterminationService.java
@@ -3,11 +3,12 @@ package uk.gov.cabinetoffice.csl.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.cabinetoffice.csl.domain.EmailUpdate;
-import uk.gov.cabinetoffice.csl.domain.Identity;
 import uk.gov.cabinetoffice.csl.domain.Reactivation;
 import uk.gov.cabinetoffice.csl.dto.VerificationCodeDetermination;
 import uk.gov.cabinetoffice.csl.exception.VerificationCodeTypeNotFound;
 
+import static java.net.URLDecoder.decode;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static uk.gov.cabinetoffice.csl.domain.ReactivationStatus.PENDING;
 import static uk.gov.cabinetoffice.csl.dto.VerificationCodeType.*;
 import static uk.gov.cabinetoffice.csl.util.TextEncryptionUtils.getDecryptedText;
@@ -40,7 +41,8 @@ public class VerificationCodeDeterminationService {
             EmailUpdate emailUpdate = emailUpdateService.getEmailUpdateRequestForCode(code);
             return new VerificationCodeDetermination(emailUpdate.getNewEmail(), EMAIL_UPDATE);
         } else {
-            String email = getDecryptedText(code, encryptionKey);
+            String encryptedEmail = decode(code, UTF_8);
+            String email = getDecryptedText(encryptedEmail, encryptionKey);
             if(identityService.isIdentityExistsForEmail(email)) {
                 return new VerificationCodeDetermination(email, ASSIGN_AGENCY_TOKEN);
             }
