89a49e713c138321697de1feca6fefb15855132b
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-03-26 10:10:53 +0000

    LC-3466-Update the login process for the blocked token users

--- src/main/java/uk/gov/cabinetoffice/csl/service/VerificationCodeDeterminationService.java
+++ src/main/java/uk/gov/cabinetoffice/csl/service/VerificationCodeDeterminationService.java
@@ -1,6 +1,5 @@
 package uk.gov.cabinetoffice.csl.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.cabinetoffice.csl.domain.EmailUpdate;
